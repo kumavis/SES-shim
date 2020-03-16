ac25636c6b95bf451a95ac3c7315036a9667fdf9
Brian Warner <warner@lothar.com>
2020-03-15 17:53:12 -0700

    feat(ses): export Compartment, next to lockdown

--- packages/ses/src/main.js
+++ packages/ses/src/main.js
@@ -12,4 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Compartment from './compartment-shim.js';
+
+export { Compartment };
 export { lockdown } from './lockdown-shim.js';
